1cce28336cb17b7b30f61e3aa25b1e08f054a963
Bevan Hunt <bevan@bevanhunt.com>
2014-02-12 11:17:26 -0800

    update package and readme

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 
 ## How to install 
 1. npm install -g meteorite (if not already installed)
-2. mrt add leaflet-awesome-markers
+2. install bootstrap 3 (e.g. mrt add bootstrap-3)
+3. install font-awesome 4 (e.g. mrt add font-awesome)
+4. mrt add leaflet-awesome-markers
 
 ## License
 MIT/X11
--- package.js
+++ package.js
@@ -3,8 +3,6 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  api.use('bootstrap-3', 'client');
-  api.use('font-awesome', 'client');
   api.add_files('lib/leaflet.awesome-markers.js', 'client');
   api.add_files('styles/leaflet.awesome-markers.css', 'client');
   api.add_files('images/markers-matte.png', 'client');
